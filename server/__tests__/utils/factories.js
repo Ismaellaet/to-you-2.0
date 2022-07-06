da4686bcc128ce7500bbb5ecd8732ef2f0c967c6
ismael <ismaelabashi@hotmail.com>
2022-07-06 19:05:39 -0300

    Add Task factory for tests

--- server/__tests__/utils/factories.js
+++ server/__tests__/utils/factories.js
@@ -1,10 +1,19 @@
 const { factory } = require("factory-girl");
-const { User } = require("../../src/app/models");
+const { User, Task } = require("../../src/app/models");
 const { faker } = require("@faker-js/faker");
 
 factory.define("User", User, {
 	username: faker.name.findName(),
 	password: faker.internet.password(),
+});
+
+factory.define("Task", Task, {
+	title: faker.random.words(3),
+	description: faker.lorem.sentences(1),
+	category: faker.word.noun(),
+	date_task: new Date(),
+	completed: faker.datatype.boolean(),
+	user_id: 1,
 });
 
 module.exports = factory;
